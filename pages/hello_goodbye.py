d0a43828fa7aa8afc520c0c572183dea104e7c68
Ira Kaufman <ira.kaufman@gmail.com>
2023-12-19 11:04:41 -0800

    Update hello_goodbye.py
    Added 3 buttons to keep scores - free throws, 2-point baskets, and 3-pointers!

--- pages/hello_goodbye.py
+++ pages/hello_goodbye.py
@@ -3,6 +3,39 @@ import streamlit as st
 st.header('st.button')
 
 if st.button('Say Hello'):
-    st.write('Why hello there')
+    st.write('Why hello there!')
 else:
-    st.write('Goodbye')
+    st.write('Bah-bye!')
+
+# if st.button('Say Hello') and st.write('Why hello there!'):
+#     st.button('Say Goodbye')
+
+
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+
+# st.write(st.session_state.score)
+st.write('')
+st.write('')
+
+col1, col2, col3 = st.columns(3)
+
+if col1.button('Free Throw'):
+    st.session_state.score += 1
+
+if col2.button('Jumper'):
+    st.session_state.score += 2
+
+if col3.button('Triple!'):
+    st.session_state.score += 3
+
+st.write(st.session_state.score)
+
+# score = 0
+#
+# st.write(score)
+#
+# if st.button('Scored'):
+#     score += 1
+#
+# st.write(score)
