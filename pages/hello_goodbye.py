cad734672a0e5454be1b41654f65a0a0ab731e30
Ira Kaufman <ira.kaufman@gmail.com>
2023-12-22 16:18:33 -0800

    Update hello_goodbye.py
    added code to grab remote IP address

--- pages/hello_goodbye.py
+++ pages/hello_goodbye.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
 
 st.header('st.button')
 
@@ -6,6 +25,9 @@ if st.button('Say Hello'):
     st.write('Why hello there!')
 else:
     st.write('Bah-bye!')
+
+results = get_remote_ip()
+st.write(results)
 
 # if st.button('Say Hello') and st.write('Why hello there!'):
 #     st.button('Say Goodbye')
